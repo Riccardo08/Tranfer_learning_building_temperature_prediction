e1502b4bf29677a1db211d97b9e04ee95ef9127a
Riccardo08 <ric.messi17@gmail.com>
2021-09-04 19:31:15 +0200

    Main_file

--- main.py
+++ main.py
@@ -162,32 +162,6 @@ print(type(val_mY), val_mY.shape)
 print(type(test_mX), test_mX.shape)
 print(type(test_mY), test_mY.shape)
 
-"""
-#_____________________________2D-CNN______________________________
-class CNN(nn.Module):
-    def __init__(self, in_channel, out_channel, kernel_size, pad=0):
-        super(CNN, self).__init__()
-        self.in_channel = in_channel
-        self.out_channel = out_channel
-        self.kernel_size = kernel_size
-        self.pad = pad
-        self.conv1 = nn.Conv2d(in_channels=self.in_channel, out_channels=self.out_channel, kernel_size=self.kernel_size, padding=self.pad)
-        self.pool = nn.MaxPool1d(2)
-        self.conv2 = nn.Conv2d(self.out_channel, 1, self.kernel_size)
-        self.fc1 = nn.Linear(20*self.kernel_size*self.kernel_size, 200) # need to change the input (20).
-        self.fc2 = nn.Linear(200, 90)
-        self.fc3 = nn.Linear(90, 1)
-
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 20*self.kernel_size*self.kernel_size) # need to change the input (20).
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-"""
 
 #_____________________________1D-CNN___________________________________
 class CNN(nn.Module):
@@ -223,7 +197,7 @@ optimizer = torch.optim.Adam(cnn_model.parameters(), lr=0.001)
 
 # Define parameters
 batch_size = 100
-epochs = 6
+epochs = 40
 
 # train_batch_size = 500
 train_data = TensorDataset(train_mX, train_mY)
@@ -296,37 +270,6 @@ plt.show()
 
 
 
-
-#=========================================================================================
-#1h PREDICTION TESTING
-"""
-test_losses = []
-# h = cnn_model.init_hidden(batch_size)
-
-cnn_model.eval()
-ypred=[]
-ylab=[]
-for inputs, labels in test_dl:
-    # h = tuple([each.data for each in h])
-    inputs = torch.reshape(inputs.float(), (batch_size, 1, inputs.shape[1]*inputs.shape[2]))
-    test_output = cnn_model(inputs.float())
-    labels = labels.unsqueeze(1)
-    test_output = test_output.detach().numpy()
-
-    #RESCALE OUTPUT
-    test_output = np.reshape(test_output, (-1, 1))
-    test_output = minT + test_output*(maxT-minT)
-
-    labels = labels.detach().numpy()
-    labels = np.reshape(labels, (-1, 1))
-
-    #RESCALE LABELS
-    labels = minT + labels*(maxT-minT)
-    ypred.append(test_output)
-    ylab.append(labels)
-"""
-
-
 #___________________________TESTING_PHASE________________________________________
 cnn_model.eval()
 
@@ -359,10 +302,25 @@ ylab = np.array(ylab, dtype=float)
 ypred = np.reshape(ypred, (-1, 1))
 ylab = np.reshape(ylab, (-1, 1))
 
-
-
 error = []
 error = ypred - ylab
+
+
+#________________________________VERIFICA: ylab == test_mY_____________________________________
+test_mY_prova = test_mY.numpy()
+for x in range(0, len(test_mY_prova)):
+    test_mY_prova[x] = minT + test_mY_prova[x]*(maxT - minT)
+
+test_mY_prova = np.reshape(test_mY_prova, (-1, 1))
+test_mY_prova = test_mY_prova[:10400]
+
+n=0
+for x in range(0, len(test_mY_prova)): #--------> n rimane = 0 => i die vettori sono gli stessi
+    if test_mY_prova[x]==test_mY[x]:
+        n += 1
+
+#______________________________________________________________________________________________
+
 
 # Plot the error
 plt.hist(error, 200, linewidth=1.5, edgecolor='black', color='orange')
@@ -374,14 +332,43 @@ plt.grid(True)
 # plt.savefig('immagini_LSTM/first_model_error.png')
 plt.show()
 
-#________VERIFICA: ylab == test_mY
-test_mY_prova = test_mY.numpy()
-for x in range(0, len(test_mY_prova)):
-    test_mY_prova[x] = minT + test_mY_prova[x]*(maxT - minT)
-test_mY_prova = np.reshape(test_mY_prova, (-1, 1))
-test_mY_prova = test_mY_prova[:10400]
-for x in range(0, len(test_mY_prova)):
 
+plt.plot(ypred, color='orange', label="Predicted")
+plt.plot(ylab, color="b", linestyle="dashed", linewidth=1, label="Real")
+plt.grid(b=True, which='major', color='#666666', linestyle='-')
+plt.minorticks_on()
+plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+plt.xlim(left=0,right=360)
+plt.ylabel('Mean Air Temperature [°C]')
+plt.xlabel('Time [h]')
+plt.title("Real VS predicted temperature", size=15)
+plt.legend()
+plt.savefig('immagini_LSTM/final_LSTM_real_VS_predicted_temperature(10_neurons).png')
+plt.show()
+
+
+#METRICS
+def mean_absolute_percentage_error(y_true, y_pred):
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+MAPE = mean_absolute_percentage_error(ylab, ypred)
+RMSE=mean_squared_error(ylab,ypred)**0.5
+R2 = r2_score(ylab,ypred)
+
+print('MAPE:%0.5f%%'%MAPE)
+print('RMSE:%0.5f'%RMSE.item())
+print('R2:%0.5f'%R2.item())
+
+plt.scatter(ylab,ypred,  color='k', edgecolor= 'white', linewidth=1,alpha=0.1)
+plt.grid(b=True, which='major', color='#666666', linestyle='-')
+plt.minorticks_on()
+plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+plt.xlabel('Real Temperature [°C]')
+plt.ylabel('Predicted Temperature [°C]')
+plt.title("Prediction distribution", size=15)
+plt.savefig('immagini_LSTM/final_LSTM_prediction_distribution(10_neurons).png')
+plt.show()
 
 
 
@@ -442,228 +429,4 @@ model_conv, feature_extractor_acc = train_model(model_conv, criterion, optimizer
 visualize_model(model_conv)
 
 
-
-
-
-
-
-
-
-
-
-"""
-#======================================== LSTM Structure ========================================#
-#HYPER PARAMETERS
-lookback = 48
-train_episodes = 10
-lr = 0.008 #0.005 #0.009
-num_layers = 5
-num_hidden = 8
-batch_size = 100
-
-
-train_batch_size = 500
-train_data = TensorDataset(train_mX, train_mY)
-train_dl = DataLoader(train_data, batch_size=batch_size, shuffle=True, drop_last=True)
-
-val_batch_size = 300
-val_data = TensorDataset(val_mX, val_mY)
-val_dl = DataLoader(val_data, batch_size=batch_size, shuffle=True, drop_last=True)
-
-test_data = TensorDataset(test_mX, test_mY)
-test_dl = DataLoader(test_data) # batch_size -> terza dimensione
-
-# Structure
-class MV_LSTM(torch.nn.Module):
-    def __init__(self, n_features, seq_length, drop_prob=0.2):
-        super(MV_LSTM, self).__init__()
-        self.seq_len = seq_length
-        self.n_hidden = num_hidden# number of hidden states
-        self.n_layers = num_layers# number of LSTM layers (stacked)
-        self.l_lstm = torch.nn.LSTM(input_size = n_features,
-                                 hidden_size = self.n_hidden,
-                                 num_layers = self.n_layers,
-                                 batch_first = True)
-        # self.dropout = torch.nn.Dropout(drop_prob)
-        # according to pytorch docs LSTM output is
-        # (batch_size,seq_len, num_directions * hidden_size)
-        # when considering batch_first = True
-        self.l_linear = torch.nn.Linear(self.n_hidden, 1)
-
-    def forward(self, x, h):
-        batch_size, seq_len, _ = x.size()
-        # hidden_state = torch.zeros(self.n_layers, batch_size, self.n_hidden)
-        # cell_state = torch.zeros(self.n_layers, batch_size, self.n_hidden)
-        lstm_out, h = self.l_lstm(x,h)
-        # h_out_numpy = h[0].detach().numpy() # se n layer = 1 all'ora h_out_numpy è ugugla a out_numpy2
-        # out_numpy = lstm_out.detach().numpy()
-        out = lstm_out[:,-1,:]
-        # out_numpy2 = out.detach().numpy()#many to one, I take only the last output vector, for each Batch
-        out_linear_transf = self.l_linear(out)
-        return out_linear_transf, h
-
-    def init_hidden(self, batch_size):
-        # even with batch_first = True this remains same as docs
-        hidden_state = torch.zeros(self.n_layers,batch_size,self.n_hidden)
-        cell_state = torch.zeros(self.n_layers,batch_size,self.n_hidden)
-        hidden = (hidden_state, cell_state) #HIDDEN is defined as a TUPLE
-        return hidden
-
-
-# create NN
-#generalize the number of features and the number of timesteps by linking them to the preprocessing
-n_features = train_mX.shape[2]
-n_timesteps = lookback
-
-#initialize the network,criterion and optimizer
-mv_net = MV_LSTM(n_features, n_timesteps)
-criterion = torch.nn.MSELoss() # reduction='sum' created huge loss value
-optimizer = torch.optim.Adam(mv_net.parameters(), lr=lr)
-
-#initialize the training loss and the validation loss
-LOSS = []
-VAL_LOSS = []
-
-#START THE TRAINING PROCESS
-mv_net.train()
-
-for t in range(train_episodes):
-
-    h = mv_net.init_hidden(batch_size)  #hidden state is initialized at each epoch
-    loss = []
-    for x, label in train_dl:
-        h = mv_net.init_hidden(batch_size) #since the batch is big enough, a stateless mode is used (also considering the possibility to shuffle the training examples, which increase the generalization ability of the network)
-        h = tuple([each.data for each in h])
-        output, h = mv_net(x.float(), h)
-        label = label.unsqueeze(1) #utilizzo .unsqueeze per non avere problemi di dimensioni
-        loss_c = criterion(output, label.float())
-        optimizer.zero_grad()
-        loss_c.backward()
-        optimizer.step()
-        loss.append(loss_c.item())
-    LOSS.append(np.sum(loss) /batch_size)
-    # print("Epoch: %d, training loss: %1.5f" % (train_episodes, LOSS[-1]))
-
-
-    # VALIDATION LOOP
-    val_loss =[]
-    h = mv_net.init_hidden(batch_size)
-    for inputs, labels in val_dl:
-        h = tuple([each.data for each in h])
-        val_output, h = mv_net(inputs.float(), h)
-        val_labels = labels.unsqueeze(1)
-        val_loss_c = criterion(val_output, val_labels.float())
-    # VAL_LOSS.append(val_loss.item())
-        val_loss.append(val_loss_c.item())
-    VAL_LOSS.append(np.sum(val_loss) /batch_size)
-    print('Epoch : ', t, 'Training Loss : ', LOSS[-1], 'Validation Loss :', VAL_LOSS[-1])
-    #print("Epoch: %d, training loss: %1.5f" % (train_episodes, VAL_LOSS[-1]))
-
-
-
-#Plot to verify validation and train loss, in order to avoid underfitting and overfitting
-plt.plot(LOSS,'--',color='r', linewidth = 1, label = 'Train Loss')
-plt.plot(VAL_LOSS,color='b', linewidth = 1, label = 'Validation Loss')
-plt.ylabel('Loss (MSE)')
-plt.xlabel('Epoch')
-plt.xticks(np.arange(0, 10, 1))
-plt.grid(b=True, which='major', color='#666666', linestyle='-')
-plt.minorticks_on()
-plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
-plt.title("Training VS Validation loss", size=15)
-plt.legend()
-# plt.savefig('immagini_LSTM/I_LSTM_Train_VS_Val_LOSS(10_epochs).png')
-plt.show()
-
-
-#TODO: =============FINE-TUNING___or____FEATURE EXTRACTION======================================
-
-
-
-#=========================================================================================
-#1h PREDICTION TESTING
-test_data = TensorDataset(test_mX, test_mY)
-test_dl = DataLoader(test_data, shuffle=False, batch_size=batch_size, drop_last=True)
-test_losses = []
-h = mv_net.init_hidden(batch_size)
-
-
-mv_net.eval()
-ypred=[]
-ylab=[]
-for inputs, labels in test_dl:
-    h = tuple([each.data for each in h])
-    test_output, h = mv_net(inputs.float(), h)
-    labels = labels.unsqueeze(1)
-    test_output = test_output.detach().numpy()
-    #RESCALE OUTPUT
-    test_output = np.reshape(test_output, (-1, 1))
-    test_output = minT + test_output*(maxT-minT)
-
-    # labels = labels.item()
-    labels = labels.detach().numpy()
-    labels = np.reshape(labels, (-1, 1))
-    #RESCALE LABELS
-    labels = minT + labels*(maxT-minT)
-    ypred.append(test_output)
-    ylab.append(labels)
-
-flatten = lambda l: [item for sublist in l for item in sublist]
-ypred = flatten(ypred)
-ylab = flatten(ylab)
-ypred = np.array(ypred, dtype=float)
-ylab = np.array(ylab, dtype = float)
-
-
-error = []
-error = ypred - ylab
-
-plt.hist(error, 50, linewidth=1.5, edgecolor='black', color='orange')
-plt.xticks(np.arange(-0.4, 0.4, 0.1))
-plt.xlim(-0.4, 0.4)
-plt.title('First model prediction error')
-# plt.xlabel('Error')
-plt.grid(True)
-# plt.savefig('immagini_LSTM/first_model_error.png')
-plt.show()
-
-
-plt.plot(ypred, color='orange', label="Predicted")
-plt.plot(ylab, color="b", linestyle="dashed", linewidth=1, label="Real")
-plt.grid(b=True, which='major', color='#666666', linestyle='-')
-plt.minorticks_on()
-plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
-plt.xlim(left=0,right=800)
-plt.ylabel('Mean Air Temperature [°C]')
-plt.xlabel('Time [h]')
-plt.title("Real VS predicted temperature", size=15)
-plt.legend()
-# plt.savefig('immagini_LSTM/I_LSTM_real_VS_predicted_temperature(10_epochs).png')
-plt.show()
-
-
-#METRICS
-def mean_absolute_percentage_error(y_true, y_pred):
-    y_true, y_pred = np.array(y_true), np.array(y_pred)
-    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
-
-MAPE = mean_absolute_percentage_error(ylab, ypred)
-RMSE=mean_squared_error(ylab,ypred)**0.5
-R2 = r2_score(ylab,ypred)
-
-print('MAPE:%0.5f%%'%MAPE)
-print('RMSE:', RMSE.item())
-print('R2:', R2.item())
-
-
-plt.scatter(ylab,ypred,  color='k', edgecolor= 'white', linewidth=1,alpha=0.1)
-plt.grid(b=True, which='major', color='#666666', linestyle='-')
-plt.minorticks_on()
-plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
-plt.xlabel('Real Temperature [°C]')
-plt.ylabel('Predicted Temperature [°C]')
-plt.title("Prediction distribution", size=15)
-# plt.savefig('immagini_LSTM/I_LSTM_prediction_distribution(10_epochs).png')
-plt.show()
-"""
 
